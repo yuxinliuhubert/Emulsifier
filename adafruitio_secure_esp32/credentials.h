397319aa536d99ec3707b4cd0308e8b096233d30
Hubert Liu <52901645+yuxinliuhubert@users.noreply.github.com>
2022-11-25 14:44:24 -0800

    MQTT

---
+++ adafruitio_secure_esp32/credentials.h
@@ -0,0 +1,11 @@
+// Adafruit IO Account Configuration
+// (to obtain these values, visit https://io.adafruit.com and click on Active Key)
+#define AIO_USERNAME "yuxinhubert"
+#define AIO_KEY      "aio_DfjR93OE8tcmfJimgZzhgKoRSa6Y"
+
+
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID "3Yellow1Brown"
+#define WLAN_PASS "Carolisqueen"
